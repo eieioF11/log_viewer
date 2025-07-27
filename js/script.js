4cf41812f6e1984eae9b9e2194fbb6da769f0604
f11 <eieioeiji0501@gmail.com>
2025-07-28 00:31:13 +0900

    [update] 不要な変数とコメントを削除

--- js/script.js
+++ js/script.js
@@ -87,8 +87,6 @@ function load() {
 function set() {
   let lines = reader.result.split("\n");
   var data = "";
-  var y_label = "";
-  var select_count = 0;
   // y_select_columns[0] = true; // time_stamp
   lines.forEach((line) => {
     var i = 0;
@@ -123,55 +121,6 @@ function set() {
     });
   }
 }
-
-// function set_pair() {
-//   let lines = reader.result.split("\n");
-//   var data = "";
-//   var min = 100000.0;
-//   var max = -100000.0;
-//   var sel_data_num = 0;
-//   var x_label = "";
-//   var y_label = "";
-//   y_select_columns[0] = false;
-//   lines.forEach((line) => {
-//     var i = 0;
-//     line.split(",").forEach((cell) => {
-//       if (y_select_columns[i] && sel_data_num < 2) {
-//         var val = parseFloat(String(cell), 10);
-//         if (!isNaN(val)) {
-//           min = Math.min(min, val);
-//           max = Math.max(max, val);
-//         }
-//         else {
-//           if (x_label == "")
-//             x_label = cell;
-//           else
-//             y_label = cell;
-//         }
-//         data += cell + ",";
-//       }
-//       i = i + 1;
-//     });
-//     if (y_select_columns[i])
-//       sel_data_num = sel_data_num + 1;
-//     data = data.slice(0, data.length - 1) + "\n";
-//   });
-//   console.log(x_label, y_label);
-//   g = new Dygraph(document.getElementById("graph"), data, {
-//     // showRoller: true,
-//     // width: 800,
-//     // height: 800,
-//     dateWindow: [min, max],
-//     valueRange: [min, max],
-//     xlabel: x_label,
-//     ylabel: y_label,
-//     drawPoints: true,
-//     drawAxesAtZero: true,
-//     pointSize: 2.0,
-//     rollPeriod: 1,
-//     // legend: "always", // 凡例常表示
-//   });
-// }
 
 function y_axis_sel() {
   // 選択されているvalueを取得
